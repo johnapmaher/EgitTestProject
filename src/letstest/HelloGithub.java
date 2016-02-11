cd33228de839899ccfabee87c99fa08d95a11c48
John Maher <johnapmaher@gmail.com>
2016-02-11 21:38:12 +0000

    added text to branch

--- src/letstest/HelloGithub.java
+++ src/letstest/HelloGithub.java
@@ -2,6 +2,6 @@ package letstest;
 
 public class HelloGithub {
 	public static void main(String[] args) {
-		System.out.println("Hello github!");
+		System.out.println("Hello github, adding for branches sake!");
 	}
 }
